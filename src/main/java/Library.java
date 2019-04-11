9e043e9cbcf99c02284bded05c0f05e50bb840cd
Johnson_ZhuangXiang <xzhuangaa@connect.ust.hk>
2019-04-11 14:06:45 +0800

    change to the Library.java

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,7 @@
 // hahaha
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("When there is a fire,commit and push");
+		System.out.println("Kim changed the code");
 	}
     public boolean someLibraryMethod() {
         return true;
