4ad8bd48f1b629d9dfc811a781fdad22d06b6273
Johnson_ZhuangXiang <xzhuangaa@connect.ust.hk>
2019-04-11 14:38:35 +0800

    change again

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,7 @@
 // hahaha
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("Kim changed the codes and fail the lab2");
+		System.out.println("Kim changed the codes and fail the lab3");
 	}
     public boolean someLibraryMethod() {
         return true;
