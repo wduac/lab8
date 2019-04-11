4a9c1ec244dfde88501f727755c416afb0f850fb
Du,Wei <1374255642@qq.com>
2019-04-11 14:41:51 +0800

    last push

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,7 @@
 // hahaha
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("Kim changed the codes and fail the lab3");
+		System.out.println("Kim changed the codes and fail the lab4");
 	}
     public boolean someLibraryMethod() {
         return true;
