9ac5897767f5193d2cd5b53f158f601dc87cb015
Du,Wei <1374255642@qq.com>
2019-04-11 14:14:16 +0800

    some changes

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,7 @@
 // hahaha
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("Kim changed the code");
+		System.out.println("Kim changed the codes and fail the lab");
 	}
     public boolean someLibraryMethod() {
         return true;
